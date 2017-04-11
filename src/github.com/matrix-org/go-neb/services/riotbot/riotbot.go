2a3d70c11714ebd260613ce72ba5f2a1060a2ead
Richard Lewis <rick@matrix.org>
2017-04-11 22:56:07 +0100

    Read tutorial workflow from config and process tutorial steps

--- src/github.com/matrix-org/go-neb/services/riotbot/riotbot.go
+++ src/github.com/matrix-org/go-neb/services/riotbot/riotbot.go
@@ -2,6 +2,14 @@
 package riotbot
 
 import (
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	yaml "gopkg.in/yaml.v2"
+
 	"github.com/matrix-org/go-neb/types"
 	"github.com/matrix-org/gomatrix"
 )
@@ -9,9 +17,49 @@ import (
 // ServiceType of the Riotbot service
 const ServiceType = "riotbot"
 
+// "Tutorial flow structure
+var tutorialConfig *TutorialConfig
+
+// Tutorial instances
+var tutorials []Tutorial
+
+// Tutorial represents the current totorial instances
+type Tutorial struct {
+	roomID      string
+	userID      string
+	currentStep int16
+	timer       *time.Timer
+}
+
+func (t Tutorial) nextStep(cli *gomatrix.Client) {
+	msg := gomatrix.TextMessage{
+		Body:    "Next tutorial step",
+		MsgType: "m.notice",
+	}
+	if _, e := cli.SendMessageEvent(t.roomID, "m.room.message", msg); e != nil {
+		log.Print("Failed to send message")
+	}
+	return
+	// return &gomatrix.TextMessage{MsgType: "m.notice", Body: response}, nil
+}
+
 // Service represents the Riotbot service. It has no Config fields.
 type Service struct {
 	types.DefaultService
+}
+
+// TutorialConfig represents the tutorial flow / steps
+type TutorialConfig struct {
+	ResourcesBaseURL string `yaml:"resources_base_url"`
+	Tutorial         struct {
+		Steps []struct {
+			Text  string        `yaml:"text"`
+			Image string        `yaml:"image"`
+			Sound string        `yaml:"sound"`
+			Video string        `yaml:"video"`
+			Delay time.Duration `yaml:"delay"`
+		} `yaml:"steps"`
+	} `yaml:"tutorial"`
 }
 
 // Commands supported:
@@ -22,10 +70,33 @@ func (e *Service) Commands(cli *gomatrix.Client) []types.Command {
 		types.Command{
 			Path: []string{"help"},
 			Command: func(roomID, userID string, args []string) (interface{}, error) {
-				return &gomatrix.TextMessage{"m.notice", "I can't help you with that"}, nil
+				response := initTutorialFlow(cli, roomID, userID)
+				return &gomatrix.TextMessage{MsgType: "m.notice", Body: response}, nil
 			},
 		},
 	}
+}
+
+func initTutorialFlow(cli *gomatrix.Client, roomID string, userID string) string {
+	delay := tutorialConfig.Tutorial.Steps[0].Delay
+	timer := time.NewTimer(time.Millisecond * delay)
+	tutorial := Tutorial{roomID: roomID, userID: userID, currentStep: 0, timer: timer}
+	tutorials = append(tutorials, tutorial)
+	go func(tutorial Tutorial) {
+		<-timer.C
+		tutorial.nextStep(cli)
+	}(tutorial)
+	log.Printf("Starting tutorial: %v", tutorial)
+	return "Starting tutorial"
+}
+
+func getScriptPath() string {
+	_, script, _, ok := runtime.Caller(1)
+	if !ok {
+		log.Fatal("Failed to get script dir")
+	}
+
+	return filepath.Dir(script)
 }
 
 func init() {
@@ -34,4 +105,14 @@ func init() {
 			DefaultService: types.NewDefaultService(serviceID, serviceUserID, ServiceType),
 		}
 	})
+
+	var tutorialConfigFileName = getScriptPath() + "/tutorial.yml"
+	tutorialConfigYaml, err := ioutil.ReadFile(tutorialConfigFileName)
+	if err != nil {
+		log.Fatalf("Failed to read tutorial yaml config file (%s): %v ", tutorialConfigFileName, err)
+	}
+	log.Printf("Config %s", tutorialConfigYaml)
+	if err = yaml.Unmarshal(tutorialConfigYaml, &tutorialConfig); err != nil {
+		log.Fatalf("Failed to unmarshal tutorial config yaml: %v", err)
+	}
 }
